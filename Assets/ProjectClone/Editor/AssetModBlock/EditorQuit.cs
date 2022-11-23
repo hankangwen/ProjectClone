1632dee455cb900aa72eb1d21247ac4789c51bbe
hankangwen <13538898378@163.com>
2022-11-23 16:08:15 +0800

    完善package.json

---
+++ Assets/ProjectClone/Editor/AssetModBlock/EditorQuit.cs
@@ -0,0 +1,23 @@
+using UnityEditor;
+
+namespace ProjectClone.AssetModBlock
+{
+    [InitializeOnLoad]
+    public class EditorQuit
+    {
+        /// <summary>
+        /// Is editor being closed
+        /// </summary>
+        static public bool IsQuiting { get; private set; }
+        static void Quit()
+        {
+            IsQuiting = true;
+        }
+
+        static EditorQuit()
+        {
+            IsQuiting = false;
+            EditorApplication.quitting += Quit;
+        }
+    }
+}
